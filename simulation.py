d9d1ba19e066d9bda8bfa0416170e27aaaf6dcf2
Galip Argun <glpargun@gmail.com>
2024-04-28 17:18:11 +0200

    improved code for daily simulation.

--- simulation.py
+++ simulation.py
@@ -2,57 +2,30 @@ import numpy as np
 import pandapower as pp
 import pandas as pd
 import pandapower.plotting as plot
-import pickle
 
 from conf.bus_config import location_buses
 from conf.connection_config import line_connections
 from conf.line_config import line_data
 
 class Simulation:
-    def __init__(self, num_buses = 160, max_battery_capacity = 40):
+    def __init__(self, num_buses=160, max_battery_capacity=40):
         self.net = pp.create_empty_network()
         self.bus_index_name_mapping = {}
-        self.simulation_inputs = {}
-
-        self.p_load = np.random.uniform(10, 20) 
-        self.q_load = np.random.uniform(5, 10)
-
-        self.p_gen = np.random.uniform(self.p_load + 5, self.p_load + 10)
-        self.q_gen = np.random.uniform(self.q_load + 5, self.q_load + 10)
-        
-        self.battery_soc = {bus_index: 0.5 for bus_index in range(num_buses)}
         self.num_buses = num_buses
+        self.battery_soc = {bus_index: 0.5 for bus_index in range(num_buses)}
         self.battery_capacity = {bus_index: max_battery_capacity for bus_index in range(num_buses)}
 
     def create_std_line(self):
         pp.create_std_type(self.net, line_data, name="Al/St 240/40 4-bundle 380.0", element='line')
 
     def initialize_network(self):
-
         for key, value in location_buses.items():
-            # print(key, value[0])
-            bus_index = pp.create_bus(self.net, 
-                            vn_kv=380., 
-                            name=f"Bus {key}", 
-                            geodata = (value[0], value[1]),
-                            zone = value[2],
-                            max_vm_pu = np.inf, 
-                            min_vm_pu = 0.0)
+            bus_index = pp.create_bus(self.net, vn_kv=380., name=f"Bus {key}", geodata=(value[0], value[1]), zone=value[2], max_vm_pu=np.inf, min_vm_pu=0.0)
             self.bus_index_name_mapping[bus_index] = f"Bus {key}"
-            
             if value[2] == 'extgrid':
-                pp.create_ext_grid(self.net,
-                                   bus=bus_index, name=key)
-                self.bus_index_name_mapping[bus_index] = f"Bus {key}"
-            else:                
-                pp.create_sgen(self.net,
-                               bus=bus_index,
-                               p_mw=self.p_gen,
-                               q_mvar=self.q_gen,
-                               name=f"sgen {key}")
-        # print(self.bus_index_name_mapping)
-
-        return self.net
+                pp.create_ext_grid(self.net, bus=bus_index, name=key)
+            else:
+                pp.create_sgen(self.net, bus=bus_index, p_mw=np.random.uniform(10, 20), q_mvar=np.random.uniform(5, 10), name=f"sgen {key}")
     
     def initialize_connection(self):
         for line in line_connections:
@@ -61,104 +34,103 @@ class Simulation:
             length_km = line.get("length_km", 1)
             std_type = line["std_type"]
             num_parallel = max(line.get("num_parallel", 1), 1)
-            
             from_bus_idx = pp.get_element_index(self.net, "bus", f"Bus {from_bus}")
             to_bus_idx = pp.get_element_index(self.net, "bus", f"Bus {to_bus}")
+            pp.create_line(self.net, from_bus=from_bus_idx, to_bus=to_bus_idx, length_km=length_km, std_type=std_type, parallel=num_parallel)
 
-            # print(f"from: {from_bus_idx}, to: {to_bus_idx}")
-
-            pp.create_line(self.net,
-                           from_bus = from_bus_idx,
-                           to_bus = to_bus_idx,
-                           length_km=length_km,
-                           std_type=std_type,
-                           parallel=num_parallel)
-        
-        # print(self.net.line)
-            
     def update_sgens_with_battery(self):
-        charge_rate = 0.1
-        discharge_rate = 0.1
+        battery_rate = np.random.choice([-0.1, 0.1])
         soc_min = 0.3
-        soc_max = 0.7 
-        
+        soc_max = 0.7
         for bus_index in range(self.num_buses):
             sgens_at_bus = self.net.sgen[self.net.sgen.bus == bus_index]
-            
             if not sgens_at_bus.empty:
                 sgen_id = sgens_at_bus.index[0]
                 current_soc = self.battery_soc[bus_index]
                 battery_capacity_mwh = self.battery_capacity[bus_index]
-
                 if current_soc < soc_min:
-                    charge_amount = charge_rate * battery_capacity_mwh
+                    charge_amount = battery_rate * battery_capacity_mwh
                     self.net.sgen.at[sgen_id, 'p_mw'] += charge_amount
                     current_soc += charge_amount / battery_capacity_mwh
-                
                 elif current_soc > soc_max:
-                    discharge_amount = discharge_rate * battery_capacity_mwh
+                    discharge_amount = battery_rate * battery_capacity_mwh
                     self.net.sgen.at[sgen_id, 'p_mw'] -= discharge_amount
                     current_soc -= discharge_amount / battery_capacity_mwh
-                
-                current_soc = min(max(current_soc, soc_min), soc_max) # be sure that soc is specified bounds.
-                self.battery_soc[bus_index] = current_soc
+                self.battery_soc[bus_index] = min(max(current_soc, soc_min), soc_max)
 
-                P_gen = self.net.sgen.at[sgen_id, 'p_mw']
-                S_gen = np.sqrt(P_gen**2 + self.net.sgen.at[sgen_id, 'q_mvar']**2)
-                delta = np.random.choice([-1, 1]) 
-                Q_gen = delta * np.sqrt(max(0, S_gen**2 - P_gen**2))
-                self.net.sgen.at[sgen_id, 'q_mvar'] = Q_gen
-
-                self.simulation_inputs[bus_index] = {
-                'p_gen': P_gen,
-                'q_gen': Q_gen,
-                'p_load': self.p_load,
-                'q_load': self.q_load,
-                'soc': current_soc,              
-                'battery_capacity_mwh': battery_capacity_mwh  
-                }
-
-                # print("Keys in self.battery_soc:", self.battery_soc.keys())
-                # print("Keys in self.simulation_inputs:", self.simulation_inputs.keys())
-
-           
     def add_loads(self):
         for bus_index in self.bus_index_name_mapping.keys():
-            pp.create_load(self.net, bus=bus_index, p_mw=self.p_load, q_mvar=self.q_load, name=f"Load at {self.bus_index_name_mapping[bus_index]}")
-            self.simulation_inputs[bus_index] = {'p_gen': 0, 'q_gen': 0, 'p_load': self.p_load, 'q_load': self.q_load}
+            pp.create_load(self.net, bus=bus_index, p_mw=np.random.uniform(10, 20), q_mvar=np.random.uniform(5, 10), name=f"Load at {self.bus_index_name_mapping[bus_index]}")
 
-    def plot_network(self):
-        plot.simple_plot(self.net, show_plot=True)
+    def update_hourly_changes(self):
+        for idx, bus_idx in enumerate(self.net.load.index):
+            self.net.load.at[bus_idx, 'p_mw'] = np.random.uniform(10, 20)
+            self.net.load.at[bus_idx, 'q_mvar'] = np.random.uniform(5, 10)
+        for idx, bus_idx in enumerate(self.net.sgen.index):
+            self.net.sgen.at[bus_idx, 'p_mw'] = np.random.uniform(15, 30)
+            self.net.sgen.at[bus_idx, 'q_mvar'] = np.random.uniform(10, 15)
 
     def run_simulation(self):
         self.update_sgens_with_battery()
-        pp.runpp(self.net) 
+        pp.runpp(self.net)
+        P_line = self.net.res_line['p_from_mw'].values
+        Q_line = self.net.res_line['q_from_mvar'].values
+        V = self.net.res_bus['vm_pu'].values
+        target = np.hstack([P_line, Q_line, V])[np.newaxis, :]
+        P_G = self.net.sgen['p_mw'].values
+        Q_G = self.net.sgen['q_mvar'].values
+        P_L = self.net.load['p_mw'].values
+        Q_L = self.net.load['q_mvar'].values
+        inputs = np.hstack([P_G, Q_G, P_L, Q_L])[np.newaxis, :]
+        return target, inputs
 
-        P_line = self.net.res_line['p_from_mw'].tolist()
-        Q_line = self.net.res_line['q_from_mvar'].tolist()
-        V = self.net.res_bus['vm_pu'].tolist()
+    def create_dataset(self, days=2):
+        X, Y = [], []
+        print(f"Starting dataset creation for {days} days, each with 24 hours.")
+        for day in range(days):
+            for hour in range(24):
+                print(f"Day {day + 1}, Hour {hour + 1}: Updating loads and generations.")
+                self.update_hourly_changes()  # Update network conditions for the current hour
+                target, inputs = self.run_simulation()
+                Y.append(target)
+                X.append(inputs)
+            print(f"Completed simulations for Day {day + 1}.")
+        self.Y = np.vstack(Y)
+        self.X = np.vstack(X)
+        print("Dataset creation complete.")
 
-        y = [P_line, Q_line, V]
+    def save_dataset(self):
+        print("Saving dataset to CSV")
+        X_cols, Y_cols = self.get_columns()
+        Y_df = pd.DataFrame(data=self.Y, columns=Y_cols)
+        X_df = pd.DataFrame(data=self.X, columns=X_cols)
+        try:
+            Y_df.to_csv('./data/Y_PF.csv', index=False)
+            X_df.to_csv('./data/X_PF.csv', index=False)
+            print("Datasets saved successfully")
+        except Exception as e:
+            print(f"Failed to save datasets: {e}")
 
-        print("\nSimulation output Y:")
-        print(y)
-        # print(self.net.res_line)
-        print("\nSimulation inputs X:")
-        x = []
-        for bus_id, inputs in self.simulation_inputs.items():
-            bus_inputs = [inputs['p_gen'], inputs['q_gen'], inputs['p_load'], inputs['q_load']]
-            x.append(bus_inputs)
-        # print(x)
-        print(self.simulation_inputs)
-    
-
+    def get_columns(self):
+        X_cols, Y_cols = [], []
+        for i in range(len(self.net.sgen)):
+            X_cols.append(f"P_G{i}")
+            X_cols.append(f"Q_G{i}")
+        for i in range(len(self.net.bus)):
+            X_cols.append(f"P_L{i}")
+            X_cols.append(f"Q_L{i}")
+        for idx, (i, j) in enumerate(self.net.line[['from_bus', 'to_bus']].values):
+            Y_cols.append(f"P_{i}_{j}")
+            Y_cols.append(f"Q_{i}_{j}")
+        for i in range(len(self.net.bus)):
+            Y_cols.append(f"V_{i}")
+        return X_cols, Y_cols
 
 if __name__ == '__main__':
     sim = Simulation()
     sim.create_std_line()
     sim.initialize_network()
     sim.initialize_connection()
-    # sim.plot_network()
     sim.add_loads()
-    sim.update_sgens_with_battery()
-    sim.run_simulation()
+    sim.create_dataset()
+    sim.save_dataset()
